ce43c802615e1673bbc8dbe1efbbcca2e878c6f3
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-05 20:29:02 +0400

    add new components and some codes in navbar component

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import "./Navbar.scss"
+import { Link } from 'react-router-dom'
+import Search from '../search/Search'
+
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <nav>
+            <ul className="item_ul">
+                <li className="item_li">
+                    <Link to="/man">Man</Link>
+                    <Link to="/Woman">Woman</Link>
+                    <Link to="/Shop">Shop</Link>
+                    <Link to="/Accessory">Accessory</Link>
+                    <Link to="/About">About</Link>
+                </li>
+            </ul>
+
+            <div className="logo">
+                <h1>RAAD SHOP</h1>
+            </div>
+
+            <div className="right">
+                <Search/>
+            </div>
+        </nav>
+      
+    </div>
+  )
+}
+
+export default Navbar
