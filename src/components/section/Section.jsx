9aba38ee0bdd0dd8a23796437702e413d7210415
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-07 20:08:14 +0400

    add new codes

---
+++ src/components/section/Section.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Section.scss";
+
+
+const Section = () => {
+  return (
+    <section className='section'>
+        <div className="content">
+            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+            <button>BUY NOW</button>
+        </div>
+
+        <div className="text_btn">
+            <p>10% Lorem ipsum dolor sit amet.</p>
+        </div>
+      
+    </section>
+  )
+}
+
+export default Section
