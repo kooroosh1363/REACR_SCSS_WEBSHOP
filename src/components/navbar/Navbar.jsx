5eb7f8358c8304f939f6afde0df6f6649a9c8659
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-07 08:22:35 +0400

    add css codes for navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 import "./Navbar.scss"
 import { Link } from 'react-router-dom'
 import Search from '../search/Search'
-
-
+import logo from "../../assets/image/raad.png";
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className='navbar '>
         <nav>
             <ul className="item_ul">
                 <li className="item_li">
                     <Link to="/man">Man</Link>
                     <Link to="/Woman">Woman</Link>
                     <Link to="/Shop">Shop</Link>
-                    <Link to="/Accessory">Accessory</Link>
+                    <Link to="/Accessory" className='color'>Accessory</Link>
                     <Link to="/About">About</Link>
                 </li>
             </ul>
 
             <div className="logo">
                 <h1>RAAD SHOP</h1>
+                {/* <img src={logo} alt="" /> */}
             </div>
 
             <div className="right">
@@ -32,4 +32,4 @@ const Navbar = () => {
   )
 }
 
-export default Navbar
+export default Navbar;
